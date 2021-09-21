c4872415b2e5b05579f6df7fae8a03b900c14140
Tristan <75189508+realTristan@users.noreply.github.com>
2021-09-21 14:38:47 -0400

    Update examples.py

--- docs/examples.py
+++ docs/examples.py
@@ -1,4 +1,4 @@
-import requests, base64
+import requests, base64, random, string
 
 class UbiAPI(object):
     def __init__(self, auth):
@@ -79,11 +79,11 @@ class UbiAPI(object):
     def change_name(self, name=None, account=None, proxies=None):
         if account is not None:
             login = self.login(account=account, proxies=proxies)
-            headers=self.headers
-            headers["Authorization"] = login[1]
+            self.session_headers["ubi-sessionid"] = login[2]
+            self.session_headers["Authorization"] = login[1]
 
-        check_1 = self.session.post(f"https://public-ubiservices.ubi.com/v3/profiles/{login[0]['userId']}/validateUpdate", data={"nameOnPlatform": name}, headers=headers, proxies=proxies)
-        check_2 = self.session.put("https://public-ubiservices.ubi.com/v3/profiles/", data={"nameOnPlatform": name}, headers=headers, proxies=proxies)
+        check_1 = self.session.post(f"https://public-ubiservices.ubi.com/v3/profiles/{login[0]['userId']}/validateUpdate", data={"nameOnPlatform": name}, headers=self.session_headers, proxies=proxies)
+        check_2 = self.session.put("https://public-ubiservices.ubi.com/v3/profiles/", data={"nameOnPlatform": name}, headers=self.session_headers, proxies=proxies)
 
         return [check_1.json(), check_2.json()] # RETURNS LIST INDEX [0, 1]
 
@@ -112,10 +112,15 @@ class UbiAPI(object):
 
 
 if __name__ == "__main__":
+    # LOADING API
+    # /////////////////
     account = "email:password"
     ubi = UbiAPI(account)
     auth_token = ubi.auth
 
+
+    # EXAMPLES
+    # /////////////////
     print(
         ubi.get_user_by_name(
             name="godly", 
@@ -129,5 +134,44 @@ if __name__ == "__main__":
             account=account,
             proxies=None,
             new_login=True
+        )
+    )
+
+    print(
+        ubi.login(
+            account=account, 
+            proxies=None
+        )
+    )
+
+    print(
+        ubi.change_name(
+            account=account,
+            name="random name",
+            proxies=None
+        )
+    )
+
+    print(
+        ubi.get_avatar(
+            user_id="user id",
+            proxies=None
+        )
+    )
+
+    print(
+        ubi.get_user_by_id(
+            user_id='user id',
+            proxies=None
+        )
+    )
+
+    name = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(7))
+    print(
+        ubi.create_account(
+            name=name,
+            email=name + ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(7)) + "@gmail.com",
+            password=name + ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(7)),
+            proxies=None
         )
     )
