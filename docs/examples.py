16688f2886cacfc872ea267af744ff644e82c886
Tristan <75189508+realTristan@users.noreply.github.com>
2021-09-21 21:44:17 -0400

    Create examples.py

---
+++ docs/examples.py
@@ -0,0 +1,65 @@
+from .api import *
+
+# LOADING API
+# /////////////////
+account = "email:password"
+ubi = UbiAPI(account)
+auth_token = ubi.auth
+
+
+# EXAMPLES
+# /////////////////
+print(
+    ubi.get_user_by_name(
+        name="godly", 
+        proxies=None
+    )
+)
+
+print(
+    ubi.add_friend(
+        friend_name="godly",
+        account=account,
+        proxies=None,
+        new_login=True
+    )
+)
+
+print(
+    ubi.login(
+        account=account, 
+        proxies=None
+    )
+)
+
+print(
+    ubi.change_name(
+        account=account,
+        name="random name",
+        proxies=None
+    )
+)
+
+print(
+    ubi.get_avatar(
+        user_id="user id",
+        proxies=None
+    )
+)
+
+print(
+    ubi.get_user_by_id(
+        user_id='user id',
+        proxies=None
+    )
+)
+
+name = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(7))
+print(
+    ubi.create_account(
+        name=name,
+        email=name + ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(7)) + "@gmail.com",
+        password=name + ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(7)),
+        proxies=None
+    )
+)
